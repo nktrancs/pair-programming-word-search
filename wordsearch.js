c6c735d6d62a284b480928cf6e4713cdf6382f4e
Nhan <nktran4@gmail.com>
2022-04-14 09:34:41 -0400

    complete

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    let result = [];
+  
+    for (let i = 0; i < letters[0].length; i++) {
+      result.push([]);
     }
-}
+    for (let j = 0; j < letters.length; j++) {
+      for (let i = 0; i < letters[0].length; i++) {
+        result[i].push(letters[j][i]);
+      }
+    }
+    const verticalJoin =  result.map(vert => vert.join(''));
+      
+  
+    for (let l of horizontalJoin) {
+      if (l.includes(word)) return true;
+    }
+  
+    for (let vert of verticalJoin) {
+      if (vert.includes(word)) return true;
+    }
+    return false;
+  };
 
-module.exports = wordSearch
+module.exports = (wordSearch)
+  
+
+  
